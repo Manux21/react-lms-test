110c57027f6b0b17ef8e70ee48e6b9f6842aab35
Manux21 <Manux21@yandex.ru>
2022-10-05 23:49:03 +0300

    creating registration page, custom input and button components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Button.module.css'
+
+
+const Button = ({text}) => {
+  return (
+    <>
+      <button>{text}</button>
+    </>
+  );
+};
+
+export default Button;
