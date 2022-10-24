f788a832cda837f5fb3d7cdf6f88d204a5b41290
Manux21 <Manux21@yandex.ru>
2022-10-24 23:59:25 +0300

    updated

--- src/components/UserBoard/UserBoard.jsx
+++ src/components/UserBoard/UserBoard.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import classes from './UserBoard.module.css'
+import AlarmBell from '../../svg/Alarm Bell.svg'
+import Avatar from '../../img/Avatar.png'
+import ArrowDown from '../../svg/ArrowDown.svg'
 
 const MyComponent = () => {
   return (
     <div className={classes.userBoard}>
-      <h1>Hello world!</h1>
+      <div className={classes.userHeader}>
+
+          <img className={classes.notification} src={AlarmBell} alt=""/>
+
+          <div className={classes.profile}>
+            <img className={classes.avatar} src={Avatar} alt="avatar"/>
+            <p>Ivan Egorov</p>
+            <img className={classes.arrowDown} src={ArrowDown} alt="ArrowDown"/>
+          </div>
+
+      </div>
     </div>
   );
 };
